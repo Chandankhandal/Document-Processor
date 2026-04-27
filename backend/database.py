dfd21a8b68248ee5be263f59ecc8b6656fea6f6b
neeraj <neerajkhandel02@gmail.com>
2026-04-28 03:43:02 +0530

    Allow dynamic database URL for production

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine, Column, Integer, String, JSON
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-# URL to connect to the Docker Postgres container
-DATABASE_URL = "postgresql://admin:password123@localhost:5432/document_vault"
+# --- CHANGE THIS PART ---
+# It tries to find the 'DATABASE_URL' environment variable first.
+# If it's not found (like when you are working locally), it uses your Docker URL.
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://admin:password123@localhost:5432/document_vault")
+# ------------------------
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
